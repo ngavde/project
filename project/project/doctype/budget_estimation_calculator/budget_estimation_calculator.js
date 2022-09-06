a35b3d3ca676c6a762c6a19fbfdfbe74f6a203ad
Vaibhav Solanke <vsolanke@dexciss.com>
2022-09-06 14:00:00 +0530

    Amt Calculation

--- project/project/doctype/budget_estimation_calculator/budget_estimation_calculator.js
+++ project/project/doctype/budget_estimation_calculator/budget_estimation_calculator.js
@@ -6,3 +6,21 @@ frappe.ui.form.on('Budget Estimation Calculator', {
 
 	// }
 });
+
+frappe.ui.form.on('Budget Estimation Calculator Table1', {
+	rate: function(frm,cdt,cdn) {
+		var child = locals[cdt][cdn]
+		frappe.call({
+			method:'amt_calc',
+			doc:cur_frm.doc,
+			callback: function(r){
+				console.log("kkkkkk",r.message)
+				child.amount = r.message
+				// cur_frm.set_value(child.amount,r.message)
+				cur_frm.refresh_field(child.amount)
+			}
+
+		})
+
+	}
+});
--- project/project/doctype/budget_estimation_calculator/budget_estimation_calculator.py
+++ project/project/doctype/budget_estimation_calculator/budget_estimation_calculator.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2022, MIT and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class BudgetEstimationCalculator(Document):
-	pass
+	@frappe.whitelist()
+	def amt_calc(self):
+		if self.table_1:
+			for i in self.table_1:
+				print('55555555555555555',i.qty,i.rate)
+				return i.qty * i.rate
